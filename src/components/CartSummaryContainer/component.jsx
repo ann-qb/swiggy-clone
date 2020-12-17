dddb2c4f525307fc32218a375b2d9abe759a2fdb
Ann Susan <anns@qburst.com>
2020-12-17 14:54:16 +0530

    Use connect in CartSummaryContainer

--- src/components/CartSummaryContainer/component.jsx
+++ src/components/CartSummaryContainer/component.jsx
@@ -1,27 +1,55 @@
 import { reset } from "./../../features/cart/cartSlice";
-import { useSelector, useDispatch } from "react-redux";
+import { connect } from "react-redux";
 import CartSummary from "../CartSummary/component";
 
-export default function CartSummaryContainer() {
-  const data = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const numberOfItems = data.reduce(
-    (acc, each) => (each.quantity > 0 ? (acc += each.quantity) : acc),
-    0
-  );
-  const totalPrice = data.reduce(
-    (acc, each) =>
-      each.quantity > 0 ? (acc += each.price * each.quantity) : acc,
-    0
-  );
-  const onCheckOut = () => {
-    dispatch(reset());
+const mapStateToProps = (state) => {
+  return {
+    numberOfItems: state.reduce(
+      (acc, each) => (each.quantity > 0 ? (acc += each.quantity) : acc),
+      0
+    ),
+    totalPrice: state.reduce(
+      (acc, each) =>
+        each.quantity > 0 ? (acc += each.price * each.quantity) : acc,
+      0
+    ),
   };
-  return (
-    <CartSummary
-      numberOfItems={numberOfItems}
-      totalPrice={totalPrice}
-      onCheckOut={onCheckOut}
-    />
-  );
-}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onCheckOut: () => {
+      dispatch(reset());
+    },
+  };
+};
+
+const CartSummaryContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartSummary);
+export default CartSummaryContainer;
+
+// import { reset } from "./../../features/cart/cartSlice";
+// import { useSelector, useDispatch } from "react-redux";
+// import CartSummary from "../CartSummary/component";
+
+// export default function CartSummaryContainer(props) {
+//   const data = useSelector((state) => state);
+//   const dispatch = useDispatch();
+//   const numberOfItems = data.reduce(
+//     (acc, each) => (each.quantity > 0 ? (acc += each.quantity) : acc),
+//     0
+//   );
+//   const totalPrice = data.reduce(
+//     (acc, each) =>
+//       each.quantity > 0 ? (acc += each.price * each.quantity) : acc,
+//     0
+//   );
+//   const onCheckOut = () => {
+//     dispatch(reset());
+//   };
+//   return (
+//       <CartSummary numberOfItems={numberOfItems} totalPrice={totalPrice} onCheckOut={onCheckOut}/>
+//   );
+// }
